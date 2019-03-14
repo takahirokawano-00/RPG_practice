3e6a6cd3e48fe386285c348957ce38bcccbf7419
takahirokawano-00 <47766479+takahirokawano-00@users.noreply.github.com>
2019-03-14 10:18:32 +0900

    キャラクター追加

--- src/Hoge.java
+++ src/Hoge.java
@@ -5,12 +5,14 @@ public class Hoge {
 		Character c3 = new Character("ジャイアン", 14000, 530);
 		Character c4 = new Character("スネ夫", 400, 400);
 		Character c5 = new Character("しずか", 260, 130);
+		Character c6 = new Character("出木杉", 730, 860);
 
 		c.introduce();
 		c2.introduce();
 		c3.introduce();
 		c4.introduce();
 		c5.introduce();
+		c6.introduce();
 		System.out.println("-----------------------------------");
 		c3.attack(c2);
 	}
